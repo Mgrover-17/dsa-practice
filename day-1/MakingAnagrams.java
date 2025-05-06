ceec09482a377142d2bedcc98d71e5274d0ba348
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-05-06 15:16:44 +0530

    [Manvi Grover] ADD: anagram problem

---
+++ day-1/MakingAnagrams.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'makingAnagrams' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static int makingAnagrams(String s1, String s2) {
+        // Write your code here
+        int freqArray[]=new int[26];
+        for(char ch:s1.toCharArray()){
+            freqArray[ch-'a']++;
+        }
+        for(char ch:s2.toCharArray()){
+            freqArray[ch-'a']--;
+        }
+        int deletions=0;
+        for(int i=0;i<26;i++){
+            deletions+=Math.abs(freqArray[i]);
+        }
+        return deletions;
+
+
+    }
+
+}
+
+public class MakingAnagrams {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s1 = bufferedReader.readLine();
+
+        String s2 = bufferedReader.readLine();
+
+        int result = Result.makingAnagrams(s1, s2);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
